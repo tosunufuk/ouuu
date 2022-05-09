60d0be0245780c3b017b066b687ccb16f8ef7895
Ufuk Tosun <tosunufuk@outlook.com>
2022-05-09 16:09:46 +0300

    icon, background changed, links added.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from "./logo.svg";
-import background from "./comingSoon.jpg";
+// import logo from "./logo.svg";
+// import background from "./comingSoon.jpg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img className="ImageContainer" src={background} />
+        <div className="ImageContainer">
+          <div className="Links">
+            <a
+              href="https://www.linkedin.com/in/tosunufuk/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+            <a
+              href="https://github.com/tosunufuk/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+          </div>
+        </div>
+        {/* <img className="ImageContainer" src={background} /> */}
       </header>
     </div>
   );
